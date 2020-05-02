98ee664d707c86f819d7df2e77fe54ae95ec7090
Sy Rashid <sy.rashid.11@gmail.com>
2020-05-02 22:06:37 +1000

    FIX: Fixed javascript to enable delete on link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'plant_tags/new'
-  get 'plant_tags/create'
   resources :gardens do
     resources :plants, only: :create
   end
   resources :plants, only: :destroy
+
   resources :plants, only: [] do
     resources :plant_tags, only: [:new, :create]
   end
-
 end
